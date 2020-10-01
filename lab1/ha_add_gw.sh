561bed542dec6276e6df5a8a9db054abd5be805a
root <root@node.benglert-qv79703.cs6480-2020.emulab.net>
2020-09-30 20:59:08 -0600

    vtysh might work

--- lab1/ha_add_gw.sh
+++ lab1/ha_add_gw.sh
@@ -1,1 +1,2 @@
+# runs code to add the necessary route to ha
 route add -net 10.0.11.0/24 gw 10.0.10.3 
--- lab1/r4_south.sh
+++
@@ -1,7 +0,0 @@
-service zebra stop
-service ospfd stop
-rm /etc/quagga/ospfd.conf
-cp /files/ospfd_r4_light.conf /etc/quagga/ospfd.conf
-service zebra start
-service ospfd start
-
